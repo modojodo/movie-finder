537d998aa34754c1855238cde1b46e3682224430
Umer Hassan <umerhassan94@gmail.com>
2021-01-04 13:00:39 -0500

    improvement(Search): adding state to keep track of request - so we can disable/enable elements accordingly

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,45 +5,49 @@ class Search extends Component {
     super(props);
     this.state = {
       query: '',
+      isLoading: false
     }
   }
 
   onChange(event) {
     const { target: { value } } = event;
-    this.setQuery(value)
+    this.updateState(value, false)
   }
 
-  setQuery(query) {
+  updateState(query, isLoading) {
     this.setState({
-      query
+      query,
+      isLoading
     });
   }
 
   onSearch() {
     const { query } = this.state;
+    this.updateState(query, true);
     fetch(`http://www.omdbapi.com/?apikey=ed65566c&s=${query}`)
       .then(res => res.json())
       .then((data) => {
         const { updateResults } = this.props;
-
         data.Response ? updateResults(data.Search) : updateResults([]);
+        this.updateState('', false);
       });
-    this.setQuery('');
   }
 
   render() {
-    const { query } = this.state;
+    const { query, isLoading } = this.state;
+    console.log(isLoading)
     return (
       <div className="columns mt-2">
         <div className="column is-9">
           <div className="control">
             <input className="input is-fullwidth" type="text" placeholder="Search your movie..." value={query}
-                   onChange={this.onChange.bind(this)}/>
+                   onChange={this.onChange.bind(this)} disabled={isLoading}/>
           </div>
         </div>
         <div className="column is-3">
           <div className="control">
-            <button className="button is-primary is-fullwidth" onClick={this.onSearch.bind(this)}>
+            <button className={`button is-primary is-fullwidth ${isLoading ? 'is-loading' : ''}`}
+                    onClick={this.onSearch.bind(this)}>
               <strong>Search</strong>
             </button>
           </div>
