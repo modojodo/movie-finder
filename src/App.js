30e80462d6fa35692cea8d1eb1006c3c0437d52b
Umer Hassan <umerhassan94@gmail.com>
2021-01-04 13:35:03 -0500

    improvement:(components/App): import Search and Media components, added state to keep track of search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import './App.scss';
+import { React, Component } from 'react';
+import Search from "./components/Search/Search";
+import Media from "./components/Media/Media";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: []
+    }
+  }
+
+  updateSearchResutls(results) {
+    console.log(results);
+    this.setState({
+      search: results && results.length ? results : []
+    });
+  }
+
+  render() {
+    const { search } = this.state;
+    return (
+      <div className="App section">
+        <div className="container">
+          <Search updateResults={this.updateSearchResutls.bind(this)} />
+          <div className="columns is-multiline">
+            {
+              search.map((media) => {
+                return (
+                  <Media media={media}/>
+                );
+              })
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
